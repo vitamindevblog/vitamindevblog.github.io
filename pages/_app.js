3e34196093b34725b74da000c0a9a58896231589
vitamindevblog <107902169+veDnimatvi@users.noreply.github.com>
2025-06-10 17:33:42 +0700

    add ads

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,10 @@ export default function App({ Component, pageProps }) {
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=G-H16MTVW4PF`}
       />
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4514641971794594"
+        crossorigin="anonymous"></Script>
       <Script id="my-script" strategy="lazyOnload">
         {`
         window.dataLayer = window.dataLayer || [];
