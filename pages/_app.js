f2b05518af8c38cf4195d98d2c58a9ce9a37c890
vitamindevblog <107902169+veDnimatvi@users.noreply.github.com>
2025-06-10 10:18:01 +0700

    feat: ads

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
 import "@/styles/globals.scss";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-H16MTVW4PF`}
+      />
+      <Script id="my-script" strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-H16MTVW4PF');
+        `}
+      </Script>
+      <Component {...pageProps} />;
+    </>
+  );
 }
