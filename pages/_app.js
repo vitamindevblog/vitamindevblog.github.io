5f0aeb5c834f8ed878816db9df97e121504e648d
vitamindevblog <107902169+veDnimatvi@users.noreply.github.com>
2025-06-10 17:37:13 +0700

    add ads

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "@/styles/globals.scss";
+import Head from "next/head";
 import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
@@ -8,10 +9,6 @@ export default function App({ Component, pageProps }) {
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=G-H16MTVW4PF`}
       />
-      <Script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4514641971794594"
-        crossorigin="anonymous"></Script>
       <Script id="my-script" strategy="lazyOnload">
         {`
         window.dataLayer = window.dataLayer || [];
@@ -21,6 +18,12 @@ export default function App({ Component, pageProps }) {
         gtag('config', 'G-H16MTVW4PF');
         `}
       </Script>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4514641971794594"
+          crossorigin="anonymous"></script>
+      </Head>
       <Component {...pageProps} />;
     </>
   );
